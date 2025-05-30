97c8c834d88d6bd436af7d12e2eee195e038c480
Mr-Shehroz <mr.shehroz.khan2006>
2025-05-30 09:56:28 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Preview from "./components/preview";
 import About from "./components/about";
 import Features from "./components/features";
 import Roadmap from "./components/roadmap";
-import Milestones from "./components/milestones";
+// import Milestones from "./components/milestones";
 import Footer from "./components/footer";
 
 export default function Home() {
